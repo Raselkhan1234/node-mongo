6ab2dedc5aaae7b96c5a7928f323677e72ab26fd
Raselkhan1234 <mdrasel8045@gmail.com>
2020-12-04 05:37:27 -0800

    start node mongo

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
-const express = require('express');
-
-const app = express();
+const express=require('express')
+const app=express()
 
 app.get('/',(req,res)=>{
-    res.send('I know how to start node...YAY')
-});
-
-app.listen(4200,()=>console.log('Listening to port 4200'));
-
+    res.send('i know how to open node yay.....')
+})
+app.listen(4200,()=>console.log('num node successfully'))
